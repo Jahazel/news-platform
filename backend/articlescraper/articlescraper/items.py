ceae198f3b7b6643b708fe1dfb1b8ea0800b302d
Jahazel Sanchez <jahazelsanchez1@gmail.com>
2024-03-28 15:26:49 -0700

    update

--- backend/articlescraper/articlescraper/items.py
+++ backend/articlescraper/articlescraper/items.py
@@ -1,12 +1,7 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
-
-class ArticlescraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class CnnTechArticle(scrapy.Item):
+    title = scrapy.Field()
+    author = scrapy.Field()
+    summary = scrapy.Field()
+    url = scrapy.Field()
